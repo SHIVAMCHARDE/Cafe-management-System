336fa36afceff857625232253be9ad60199c552b
SHIVAMCHARDE <83965173+SHIVAMCHARDE@users.noreply.github.com>
2022-11-24 00:41:40 +0530

    Cards Done ( Only IMG and Ratings Remain )

--- cafe/src/Pages/Search/Cards.js
+++ cafe/src/Pages/Search/Cards.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+const heart_pic ='../Assets/heart.png';
 const Cards = ({ data }) => {
 
     return (
@@ -9,28 +9,29 @@ const Cards = ({ data }) => {
                     return (
                         <>
                             <div className="cafe_card">
+
                                 <div className='img_card_con'>
                                     <div className="cafe_img">
                                         <img src={element.imgData} alt='' />
                                     </div>
                                     <div className="info">
 
-                                        <div className='cafeName'><p id='p1'>{element.Cname}</p>
+                                        <div className='cafeName'><p id='p1'><h4 className='H4'>{element.Cname}</h4></p>
                                             <p id='p2'>{element.Food_Type}</p>
                                             < p id='p3'>{element.Address}</p>
                                         </div>
                                         <div className='ratings'>< p id='p4'>{element.Ratings}</p></div>
                                     </div>
-
-                                    
                                 </div>
-                                {/* <div className="menu">
 
-                                    <div className="like_img"><img src='' /></div>
+
+                                <div className="menu">
+                                    <div className="like_img"><img src='={require(heart_pic)}' /></div>
                                     <div className="Menu_btn">
-                                        <button placeholder='Menu'>Menu</button>
+                                        <button id='btnMenu' placeholder='Menu'>Menu</button>
                                     </div>
-                                </div> */}
+                                </div>
+
 
                             </div>
 
