ff0fdf2a90b47609384d6a01a190e3fcb9122e7e
Bhavesh Anandpara <bhaveshanandpara12@gmail.com>
2022-12-06 14:51:09 +0530

    Chef Page Css Improved

--- cafe/src/Pages/ChefPage/ChefPage.jsx
+++ cafe/src/Pages/ChefPage/ChefPage.jsx
@@ -4,31 +4,34 @@ import './ChefPage.css'
 export default function ChefPage() {
     return (
         <div>
-        <div className="OrderHeading">
-        <p>Orders Check List</p>
-      </div>
-        <div className='ChefPageCon'>
-         
-            <div className="OrderCards_Container">
+            <div className="OrderHeading">
+                <p>Orders Check List</p>
+            </div>
+            <div className='ChefPageCon'>
 
-                <div className="TblNo"><p className='PTblNo'> Table no : 1</p></div>
+                <div className="OrderCards_Container">
 
-                <div className="ItemName"><p className='PItemName'>Dish-Qty :</p>
-                    <div className="Oders">
-                        <p className='POders'>
-                            Masala Dosa -2, Veg Pizza-1
-                        </p>
+                    <p className='PTblNo TblNo'> Table no : 1</p>
+                    <p className='POrderTime OrderTime'>Order Time : 12:45 </p>
+
+                    <div className="ItemName">
+                        <div className="Oders">
+                            <p className='POders'>
+                                Masala Dosa - 2
+                            </p>
+                            <p className='POders'>
+                                Veg Pizza - 1
+                            </p>
+                        </div>
                     </div>
-                </div>
 
-                <div className="OrderTime"><p className='POrderTime'>Order Time : 12:45 </p></div>
-                <div className="BtnServed">
-                    <button type='submit' ><p className='PBtnServed'> Ready To Served </p></button>
+                    <div className="BtnServed">
+                        <button type='submit' className='PBtnServed' >Order Complete</button>
+                    </div>
+
                 </div>
 
             </div>
-
-        </div>
         </div>
     )
 }
